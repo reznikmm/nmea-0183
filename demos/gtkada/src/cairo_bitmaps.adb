82bfd23b1cacfdbc5aa6ac790505ffa4ce552c04
Maxim Reznik <reznikmm@gmail.com>
2024-07-07 12:56:38 +0300

    Check with REUSE

--- demos/gtkada/src/cairo_bitmaps.adb
+++ demos/gtkada/src/cairo_bitmaps.adb
@@ -1,3 +1,8 @@
+--  SPDX-FileCopyrightText: 2024 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+----------------------------------------------------------------
+
 with Glib;
 with Bitmap_Color_Conversion;
 
--- demos/gtkada/src/cairo_bitmaps.ads
+++ demos/gtkada/src/cairo_bitmaps.ads
@@ -1,3 +1,8 @@
+--  SPDX-FileCopyrightText: 2024 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+----------------------------------------------------------------
+
 with HAL.Bitmap;
 with Cairo;
 with Soft_Drawing_Bitmap;
